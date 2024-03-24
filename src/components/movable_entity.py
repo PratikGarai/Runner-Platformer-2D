1bb8821fb7d4a56e12579c05cddac8c3a84e7a52
PratikGarai <pratikgarai0208@gmail.com>
2024-03-24 09:34:25 +0530

    created movable entity base class

---
+++ src/components/movable_entity.py
@@ -0,0 +1,34 @@
+from pygame import Surface
+
+from src.base import BaseComponent
+from src.constants import GAME_Y
+from src.lib.sprite_roller import SpriteRoller
+
+
+class MovableEntity(BaseComponent):
+    def __init__(self, screen: Surface, height: int, stride: int, initial_x: int, initial_y: int, initial_x_speed: int, initial_y_speed: int, update_rate: int):
+        super().__init__(screen)
+
+        self.height = height
+        self.stride = stride
+
+        self.current_roller: SpriteRoller | None = None
+        self.frame = 0
+        self.update_rate = update_rate
+
+        self.entity_x_offset = initial_x
+        self.entity_y_offset = initial_y
+        self.entity_x_speed = initial_x_speed
+        self.entity_y_speed = initial_y_speed
+
+    def update(self):
+        if self.current_roller:
+            entity_sprite = self.current_roller.get_sprite()
+            x_pos = self.entity_x_offset
+            y_pos = GAME_Y - self.entity_y_offset - self.height
+
+            self.screen.blit(entity_sprite, (x_pos, y_pos))
+            self.frame += 1
+            if self.frame % self.update_rate == 0:
+                self.current_roller.update()
+                self.frame = self.frame % self.update_rate
--- src/components/player.py
+++ src/components/player.py
@@ -2,34 +2,20 @@ import os
 
 from pygame import Surface
 
-from src.base import BaseComponent
+from src.components.movable_entity import MovableEntity
 from src.constants import GAME_Y, PLAYER_ROOT
 from src.lib.sprite_roller import SpriteRoller
 
 PLAYER_SKIP_FRAME = 5
 PLAYER_X_OFFSET = 50
+PLAYER_HEIGHT = 128
+PLAYER_STRIDE = 128
 
 
-class Player(BaseComponent):
+class Player(MovableEntity):
     def __init__(self, screen: Surface, ground_offset: int):
-        super().__init__(screen)
-
-        self.height = 128
-        self.stride = 128
-
+        super().__init__(screen, PLAYER_HEIGHT, PLAYER_STRIDE, PLAYER_X_OFFSET,
+                         ground_offset, 0, 0, PLAYER_SKIP_FRAME)
         self.run_roller = SpriteRoller(path=os.path.join(
             PLAYER_ROOT, "run.png"), height=self.height, stride=self.stride)
         self.current_roller = self.run_roller
-
-        self.frame = 0
-        self.player_x_offset = PLAYER_X_OFFSET
-        self.player_y_offset = ground_offset
-
-    def update(self):
-        player_sprite = self.current_roller.get_sprite()
-        self.screen.blit(player_sprite, (self.player_x_offset,
-                         GAME_Y - self.player_y_offset - self.height))
-        self.frame += 1
-        if self.frame % PLAYER_SKIP_FRAME == 0:
-            self.current_roller.update()
-            self.frame = self.frame % PLAYER_SKIP_FRAME
